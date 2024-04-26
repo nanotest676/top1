074f778c2c5e177571ccc7592463866007ef5fa2
nano <nickbatmanll@gmail.com>
2024-04-26 22:24:27 +0300

    yes

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import UniqueConstraint
+
 
 class User(AbstractUser):
     USERNAME_FIELD = 'email'
@@ -18,3 +20,26 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.email
+
+
+class Subscribe(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='subscriber',
+        verbose_name="Подписчик",
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='subscribing',
+        verbose_name="Автор",
+        on_delete=models.CASCADE,
+    )
+    
+    class Meta:
+        ordering = ['-id']
+        constraints = [
+            UniqueConstraint(fields=['user', 'author'], name='unique_subscription')
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
