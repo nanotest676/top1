10072721f300e3ad485be96ea7bcf9e53fe98868
nano <nickbatmanll@gmail.com>
2024-04-26 20:35:47 +0300

    yes

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username', #надо подправить
+        'first_name', #надо подправить
+        'last_name', #надо подправить
+    ]
+    email = models.EmailField(
+        'email address', #надо подправить
+        max_length=254,
+        unique=True,
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Пользователь' #надо подправить
+        verbose_name_plural = 'Пользователи' #надо подправить
+
+    def __str__(self):
+        return self.username
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from api.serializers import CustomUserSerializer
+from django.contrib.auth import get_user_model
+from djoser.views import UserViewSet
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+User = get_user_model()
+
+
+class CustomPagination(PageNumberPagination):
+    page_size_query_param = "limit"
+
+class CustomUserViewSet(UserViewSet):
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    pagination_class = CustomPagination
