d41809b79d504062e1c96bd1bec4606726704010
nano <nickbatmanll@gmail.com>
2024-04-28 22:22:55 +0300

    yes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,13 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 AUTH_USER_MODEL = 'users.User'
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('DOMEN', 'foodgram')]
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
