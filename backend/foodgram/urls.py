c4a973f9baada819fd9de997648db86bb2befa6e
nano <nickbatmanll@gmail.com>
2024-05-14 22:36:53 +0300

    yes

--- backend/foodgram/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path('api/', include('api.urls', namespace='api')),
-    path('admin/', admin.site.urls),
-]
